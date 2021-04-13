ac925c4045a28e6d73053ca7a8ebc8924ad260dd
DaisukeSakusabe <daisukesakusabe@DaisukeSakusabenoMacBook-Pro.local>
2021-04-14 00:51:21 +0900

    不要なテストとimportを削除

--- src/test/Redux.test.ts
+++ src/test/Redux.test.ts
@@ -1,18 +1,5 @@
-import { Dispatch } from "react";
-import { useDispatch } from "react-redux";
-import { NumberAction, NumberActionType, plus, minus } from "../actions";
+import { NumberActionType, plus, minus } from "../actions";
 import { numberState } from "../reducer";
-
-jest.mock('react-redux', () => ({
-    useDispatch: () => jest.fn(),
-}));
-
-test('NumberActionTypeを定数を利用してNumberActionオブジェクトを作る', () => {
-    const numberAction: NumberAction = {
-        type: NumberActionType.UP,
-        amount: 2
-    };
-});
 
 test('Dispatcherに渡すActionCreatorを用意する', () => {
     const plusActionCreator = plus;
