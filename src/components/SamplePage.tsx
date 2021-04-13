39b4e8e1da8522757ff4164f2c9c3571686b9119
DaisukeSakusabe <daisukesakusabe@DaisukeSakusabenoMacBook-Pro.local>
2021-04-14 01:36:56 +0900

    SamplePageに状態と状態変更関数をPropsで渡すように修正

--- src/components/SamplePage.tsx
+++ src/components/SamplePage.tsx
@@ -1,8 +1,26 @@
 import React, { FC } from 'react';
 
-const SamplePage: FC = () => 
-    <div>
-        <h1>Redux Sample</h1>
-    </div>
+type Props = {
+    amount?: number;
+    plus?: (amount: number) => void;
+    minus?: (amount: number) => void;
+};
+
+const SamplePage: FC<Props> = ({ 
+    amount = 0,
+    plus = () => undefined,
+    minus = () => undefined,
+}) => {
+
+    return (
+        <div>
+            <h1>Redux Sample</h1>
+            <p>Reduxで状態管理している数値: {amount}</p>
+            <br/>
+            <button onClick={() => plus(10)}>+</button>
+            <button onClick={() => minus(5)}>-</button>
+        </div>
+    );
+}
 
 export default SamplePage;
