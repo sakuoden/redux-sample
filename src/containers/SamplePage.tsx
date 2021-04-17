4301e9e4905683b146ba28807d3f121cc92fc279
DaisukeSakusabe <daisukesakusabe@DaisukeSakusabenoMacBook-Pro.local>
2021-04-17 15:32:09 +0900

    containerコンポーネントでcounterSliceからActionCreator関数を取得するよう修正

--- src/containers/SamplePage.tsx
+++ src/containers/SamplePage.tsx
@@ -1,12 +1,13 @@
 import { FC } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { minus, plus } from '../features/number';
+import { counterSlice } from '../features/number';
+import { NumberState } from  '../features/number';
 import SamplePage from '../components/SamplePage';
-import { NumberState } from '../features/number';
 
 const EnhancedSamplePage: FC = () => {
     const amount = useSelector<NumberState, number>((state: NumberState) => state.amount);
     const dispatch = useDispatch();
+    const { up: plus, down: minus } = counterSlice.actions;
 
     return (
         <SamplePage 
